e48e69399a7dfa8ccc298ed9f81c09a2503a096e
Nemanja Manojlovic <manojlovic911@hotmail.com>
2021-10-11 01:42:52 +0200

    Pass item id through props

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -30,7 +30,7 @@ const DUMMY_MEALS = [
 ];
 
 const AvailableMeals = () => {
-    const mealsList = DUMMY_MEALS.map(meal => {return (<MealItem key={meal.id} name={meal.name} description={meal.description}
+    const mealsList = DUMMY_MEALS.map(meal => {return (<MealItem id={meal.id} key={meal.id} name={meal.name} description={meal.description}
                                                         price={meal.price}>{meal.name}</MealItem>)});
 
     return (
