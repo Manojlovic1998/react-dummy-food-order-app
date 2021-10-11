0d1b70eeba1668ed2465f8e4d965379526d7244e
Nemanja Manojlovic <manojlovic911@hotmail.com>
2021-10-11 02:42:45 +0200

    Add Modal and Overlay to the App

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,4 +1,5 @@
 import classes from "./Cart.module.css";
+import Modal from "../UI/Modal";
 
 const Cart = props => {
     const cartItems = <ul className={classes["cart-items"]}>{[{
@@ -8,17 +9,19 @@ const Cart = props => {
         price: 12.99
     }].map(item => <li>{item.name}</li>)}</ul>;
 
-    return (<div>
-        {cartItems}
-        <div className={classes.total}>
-            <span>Total Amount</span>
-            <span>35.62</span>
-        </div>
-        <div className={classes.actions}>
-            <button className={classes['button--alt']}>Close</button>
-            <button className={classes.button}>Order</button>
-        </div>
-    </div>)
+    return (
+        <Modal>
+            {cartItems}
+            <div className={classes.total}>
+                <span>Total Amount</span>
+                <span>35.62</span>
+            </div>
+            <div className={classes.actions}>
+                <button className={classes['button--alt']}>Close</button>
+                <button className={classes.button}>Order</button>
+            </div>
+        </Modal>
+    )
 };
 
 export default Cart;
