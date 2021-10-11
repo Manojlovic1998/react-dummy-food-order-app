0c930c031722a302d2926851510b5f8e16faebaa
Nemanja Manojlovic <manojlovic911@hotmail.com>
2021-10-11 20:35:04 +0200

    Change placement of directory store in the project directory tree

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import Header from "./components/Layout/Header";
 import {useState} from "react";
 import Meals from "./components/Meals/Meals";
 import Cart from "./components/Cart/Cart";
-import CartProvider from "./components/store/CartProvider";
+import CartProvider from "./store/CartProvider";
 
 function App() {
     const [cartVisibility, setCartVisibility] = useState(false)
