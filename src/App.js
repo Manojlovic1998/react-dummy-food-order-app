a90bfef83a0071e2005fd5e9c891c51fe7f66a71
Nemanja Manojlovic <manojlovic911@hotmail.com>
2021-10-11 03:28:08 +0200

    Wrap the App JSX with context provider

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import Header from "./components/Layout/Header";
-import {Fragment, useState} from "react";
+import {useState} from "react";
 import Meals from "./components/Meals/Meals";
 import Cart from "./components/Cart/Cart";
+import CartProvider from "./components/store/CartProvider";
 
 function App() {
     const [cartVisibility, setCartVisibility] = useState(false)
@@ -15,13 +16,13 @@ function App() {
     };
 
     return (
-        <Fragment>
+        <CartProvider>
             {cartVisibility && <Cart onHideCart={hideCartHandler}/>}
             <Header onShowCart={showCartHandler}/>
             <main>
                 <Meals/>
             </main>
-        </Fragment>
+        </CartProvider>
     );
 }
 
