7c73878de26501f7863986695ffe97d1377c874b
Nemanja Manojlovic <manojlovic911@hotmail.com>
2021-10-11 20:37:44 +0200

    Use context in HeaderCartButton to display cart item amount

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,10 +1,16 @@
-import React from "react";
+import React, {useContext} from "react";
 
 import CartIcon from "../Cart/CartIcon";
+import CartContext from "../../store/cart-context";
 import classes from "./HeaderCartButton.module.css";
 
 
 const HeaderCartButton = (props) => {
+    const cartContext = useContext(CartContext);
+    const numberOfCartItems = cartContext.items.reduce((curNumber, item) => {
+        return curNumber + item.amount;
+    }, 0);
+
     return (
         <button className={classes.button} onClick={props.onClick}>
             <span className={classes.icon}>
@@ -12,7 +18,7 @@ const HeaderCartButton = (props) => {
             </span>
             <span>Your Cart</span>
             <span className={classes.badge}>
-                3
+                {numberOfCartItems}
             </span>
         </button>
     );
